603595bc79504e9d1ca81fadb47fadef2ce398ed
Joshua Fenton <joshuafenton521@gmail.com>
2025-11-21 16:54:42 -0500

    Adding initial files from previous development

---
+++ Teams.py
@@ -0,0 +1,120 @@
+import pandas as pd
+
+class Team:
+    def __init__(self, id, key, name, mascot, conference):
+        self.id = id
+        self.key = key
+        self.name = name
+        self.mascot = mascot
+        self.conference = conference
+        self.elo = 1000
+        self.wins = 0
+        self.losses = 0
+        if self.conference in ['Atlantic Coast', 'Big East', 'Big 12', 'Big Ten', 'Southeastern']:
+            self.level = 3
+        elif self.conference in ['American', 'Coastal Athletic Association', 'America East', 'Atlantic Sun', 'Conference USA', 'Big South', 'Atlantic 10', 'Mid-American', 'Big Sky', 'Summit', 'Western Athletic', 'Sun Belt', 'Big West', 'Missouri Valley', 'Horizon League', 'Ivy League', 'Metro Atlantic Athletic', 'Mid-Eastern', 'Southwestern Athletic', 'Mountain West', 'Northeast', 'Ohio Valley', 'Pac-12', 'Patriot League''Southern', 'Southland', 'West Coast']:
+            self.level = 2
+        else:
+            self.level = 1
+
+    def print_attributes(self):
+        print(f"Team ID: {self.id} \nTeam Key: {self.key} \nSchool: {self.name} \nMascot: {self.mascot} \nCurrent ELO: {self.elo} \nConference: {self.conference} \nWins: {self.wins} \nLosses: {self.losses}")
+    
+    def set_elo(self, val):
+        self.elo = val
+
+    def print_elo(self):
+        print(f'Team: {self.name} -- {self.elo}')
+
+class Division:
+    def __init__(self):
+        self.teams = {}
+
+    def add_team(self, id, key, school, mascot, conference):
+        self.teams[id] = Team(id, key, school, mascot, conference)
+
+    def print_teams(self):
+        for team in self.teams.keys():
+            if self.teams[team].elo >= 200:
+                self.teams[team].print_attributes()
+    
+    def print_elos(self):
+        for team in self.teams.keys():
+            if self.teams[team].elo >= 200:
+                self.teams[team].print_elo()
+    
+    def play_game(self, away_id, home_id, away_score, home_score, neutral_site):
+        game_level = min(self.teams[home_id].level, self.teams[away_id].level)
+        if game_level == 3:
+            k = 40
+        elif game_level == 2:
+            k = 20
+        elif game_level == 1:
+            k = 10
+        else:
+            k = 0
+        j = 0
+        # if (self.teams[home_id].conference != 0) and (self.teams[away_id].conference != 0):
+        #     j += abs(home_score - away_score) / 3
+        if self.teams[home_id].conference == 0:
+            home_elo = 100
+        else:
+            home_elo = self.teams[home_id].elo
+        if self.teams[away_id].conference == 0:
+            away_elo = 100
+        else:
+            away_elo = self.teams[away_id].elo
+        if neutral_site == 'False':
+            home_elo += 40
+        
+        rating_diff = home_elo-away_elo
+        div_400 = -1 * rating_diff / 400
+        power = 10 ** div_400
+        home_win_prob = 1 / (1 + power)
+        away_win_prob = 1 - home_win_prob
+        if home_score > away_score:
+            if self.teams[away_id].conference in ['Atlantic Coast', 'Big East', 'Big 12', 'Big Ten', 'Southeastern']:
+                j += abs(home_score - away_score) / 3
+            elif self.teams[away_id].conference in ['American', 'Coastal Athletic Association', 'America East', 'Atlantic Sun', 'Conference USA', 'Big South', 'Atlantic 10', 'Mid-American', 'Big Sky', 'Summit', 'Western Athletic', 'Sun Belt', 'Big West', 'Missouri Valley', 'Horizon League', 'Ivy League', 'Metro Atlantic Athletic', 'Mid-Eastern', 'Southwestern Athletic', 'Mountain West', 'Northeast', 'Ohio Valley', 'Pac-12', 'Patriot League''Southern', 'Southland', 'West Coast']:
+                j += abs(home_score - away_score) / 20
+            adjustment = (away_win_prob * k) + j
+            self.teams[home_id].set_elo(self.teams[home_id].elo + adjustment)
+            self.teams[away_id].set_elo(away_elo - adjustment)
+            self.teams[home_id].wins += 1
+            self.teams[away_id].losses += 1
+        else:
+            if self.teams[home_id].conference in ['Atlantic Coast', 'Big East', 'Big 12', 'Big Ten', 'Southeastern']:
+                j += abs(home_score - away_score) / 3
+            elif self.teams[home_id].conference in ['American', 'Coastal Athletic Association', 'America East', 'Atlantic Sun', 'Conference USA', 'Big South', 'Atlantic 10', 'Mid-American', 'Big Sky', 'Summit', 'Western Athletic', 'Sun Belt', 'Big West', 'Missouri Valley', 'Horizon League', 'Ivy League', 'Metro Atlantic Athletic', 'Mid-Eastern', 'Southwestern Athletic', 'Mountain West', 'Northeast', 'Ohio Valley', 'Pac-12', 'Patriot League''Southern', 'Southland', 'West Coast']:
+                j += abs(home_score - away_score) / 10
+            adjustment = (home_win_prob * k) + j
+            self.teams[home_id].set_elo(self.teams[home_id].elo - adjustment)
+            self.teams[away_id].set_elo(away_elo + adjustment)
+            self.teams[away_id].wins += 1
+            self.teams[home_id].losses += 1
+
+        return home_elo, away_elo, self.teams[home_id].elo, self.teams[away_id].elo, k
+    
+    def to_csv(self, filename):
+        results = []
+        for team in self.teams:
+            if self.teams[team].conference == 0:
+                conf = 'n/a'
+                elo = 100
+            else:
+                conf = self.teams[team].conference
+                elo = self.teams[team].elo
+            results.append({
+                'ID': self.teams[team].id,
+                'Key': self.teams[team].key,
+                'School': self.teams[team].name,
+                'Mascot': self.teams[team].mascot,
+                'Conference': conf,
+                'ELO': elo,
+                'Record': f'{self.teams[team].wins} - {self.teams[team].losses}'
+            })
+        
+        results_df = pd.DataFrame(results)
+        results_df = results_df.sort_values(by='ELO', ascending=False)
+
+        results_df.to_csv(filename)
---
+++ main.py
@@ -0,0 +1,104 @@
+import requests
+import json
+import pandas as pd
+import datetime
+from Teams import Team, Division
+
+pd.set_option('display.max_columns', None)
+pd.set_option('display.max_rows', None)
+
+
+# Will be the real logic. Don't want to waste calls.
+input_date = datetime.date.today() - datetime.timedelta(days=1)
+res = requests.get(f'https://api.sportsdata.io/v3/cbb/scores/json/GamesByDateFinal/{input_date}?key=4854866e22114b1281822c787c0c8185')
+response = json.loads(res.text)
+# print(response)
+
+games_df = pd.DataFrame(response)
+
+while input_date > datetime.date(2025, 11, 3):
+    input_date -= datetime.timedelta(days=1)
+    res = requests.get(f'https://api.sportsdata.io/v3/cbb/scores/json/GamesByDateFinal/{input_date}?key=4854866e22114b1281822c787c0c8185')
+    response = json.loads(res.text)
+    temp_df = pd.DataFrame(response)
+    games_df = pd.concat([games_df, temp_df])
+    # print(len(df))
+
+games_df.to_csv('current_games.csv')
+
+res = requests.get(f'https://api.sportsdata.io/v3/cbb/scores/json/teams?key=4854866e22114b1281822c787c0c8185')
+response = json.loads(res.text)
+teams_df = pd.DataFrame(response)
+teams_df.to_csv('teams.csv')
+print(teams_df.Conference.unique())
+
+
+# games_df = pd.read_csv('current_games.csv')
+# teams_df = pd.read_csv('teams.csv')
+
+# print(teams_df[teams_df.TeamID == 769])
+# print(teams_df[teams_df.TeamID == 224])
+
+# teams_df = teams_df.dropna(subset=['Conference'])
+
+# print(teams_df.head())
+# print(len(teams_df))
+# print(len(teams_df[teams_df.Active == True]))
+# print(sum(teams_df.Conference.value_counts()))
+
+games_df = games_df[games_df.Status == 'Final']
+games_df = games_df[['Status', 'Day', 'DateTime', 'AwayTeam', 'HomeTeam', 'AwayTeamID', 'HomeTeamID', 'AwayTeamScore', 'HomeTeamScore', 'NeutralVenue']]
+teams_df = teams_df[['TeamID', 'Key', 'School', 'Name', 'Conference']]
+teams_df.Conference = teams_df.Conference.fillna(0)
+
+print(len(games_df))
+games_df = games_df.merge(teams_df, how='left', left_on='AwayTeamID', right_on='TeamID')
+games_df.columns = ['Status', 'Day', 'DateTime', 'AwayTeam', 'HomeTeam', 'AwayTeamID', 'HomeTeamID', 'AwayTeamScore', 'HomeTeamScore', 'NeutralVenue', 'AwayID', 'AwayKey', 'AwaySchool', 'AwayName', 'AwayConference']
+print(len(games_df))
+games_df = games_df.merge(teams_df, how='left', left_on='HomeTeamID', right_on='TeamID')
+games_df.columns = ['Status', 'Day', 'DateTime', 'AwayTeam', 'HomeTeam', 'AwayTeamID', 'HomeTeamID', 'AwayTeamScore', 'HomeTeamScore', 'NeutralVenue', 'AwayID', 'AwayKey', 'AwaySchool', 'AwayName', 'AwayConference', 'HomeID', 'HomeKey', 'HomeSchool', 'HomeName', 'HomeConference']
+print(len(games_df))
+# print(games_df.head())
+# print(teams_df.head())
+
+# test_team = Team(teams_df['TeamID'].iloc[0], teams_df['Key'].iloc[0], teams_df['School'].iloc[0], teams_df['Name'].iloc[0])
+# test_team.print_attributes()
+
+# test_division = Division()
+# test_division.add_team(teams_df['TeamID'].iloc[0], teams_df['Key'].iloc[0], teams_df['School'].iloc[0], teams_df['Name'].iloc[0])
+# test_division.add_team(teams_df['TeamID'].iloc[1], teams_df['Key'].iloc[1], teams_df['School'].iloc[1], teams_df['Name'].iloc[1])
+# test_division.print_teams()
+
+division = Division()
+
+for i in range(len(teams_df)):
+    division.add_team(teams_df['TeamID'].iloc[i], teams_df['Key'].iloc[i], teams_df['School'].iloc[i], teams_df['Name'].iloc[i], teams_df['Conference'].iloc[i])
+
+# division.teams[101].print_attributes()
+
+print(division.play_game(101, 1, 10, 20, 0))
+# print(games_df.loc[399])
+
+for i in range(len(games_df)):
+    division.play_game(games_df.AwayTeamID.iloc[i], games_df.HomeTeamID.iloc[i], games_df.AwayTeamScore.iloc[i], games_df.HomeTeamScore.iloc[i], games_df.NeutralVenue.iloc[i])
+#     if (games_df.AwayTeamID.iloc[i] == 101) or (games_df.HomeTeamID.iloc[i] == 101):
+#         print('Game ' + str(i))
+#         division.teams[games_df.AwayTeamID.iloc[i]].print_attributes()
+#         print('vs.')
+#         division.teams[games_df.HomeTeamID.iloc[i]].print_attributes()
+
+# division.print_elos()
+
+division.to_csv('current_elos.csv')
+
+# division.print_teams()
+
+# print(datetime.date.today() > datetime.date(2025, 11, 20))
+
+# print(teams_df.head())
+# print(str(datetime.date.today() - datetime.timedelta(days=1)))
+
+
+# print(games_df[games_df.AwaySchool.isna()])
+# print(games_df.head())
+# print(len(games_df))
