d2078abf543248dae3321beacb59fc62f6de5b16
Joshua Fenton <joshuafenton521@gmail.com>
2025-12-05 11:06:30 -0500

    Testing

--- testing.py
+++ testing.py
@@ -1,4 +1,5 @@
 import random
+import pandas as pd
 
 win_odds = [325, 325, 285, 400, 450]
 my_win_prob = [.396, .332, .296, .224, .218]
@@ -31,3 +32,8 @@ def run_n_times(n, func, odds_list, prob_list):
 
 
 print(run_n_times(10, play_all_games_no_parlay, win_odds, my_win_prob))
+
+
+current_games_df = pd.read_csv('current_ncaa_games.csv')
+max_date = max(current_games_df['Day'])
+print(max_date)
