3191a214d4707982ef58bb5ef7222198907388fc
Joshua Fenton <joshuafenton521@gmail.com>
2025-12-05 10:23:31 -0500

    Daily

---
+++ testing.py
@@ -0,0 +1,33 @@
+import random
+
+win_odds = [325, 325, 285, 400, 450]
+my_win_prob = [.396, .332, .296, .224, .218]
+
+def play_game(odds, prob):
+    result = random.random()
+    if result <= prob:
+        return (odds)/100
+    else:
+        return -1
+
+def play_all_games_no_parlay(odds_list, prob_list):
+    total = 0
+    any_wins = False
+    for i in range(len(odds_list)):
+        game_result = play_game(odds_list[i], prob_list[i])
+        if game_result != -1:
+            any_wins = True
+        total += game_result
+    if any_wins == False:
+        None
+    return total
+
+def run_n_times(n, func, odds_list, prob_list):
+    results = []
+    for i in range(n):
+        results.append(func(odds_list, prob_list))
+    return sum(results) / n
+
+
+
+print(run_n_times(10, play_all_games_no_parlay, win_odds, my_win_prob))
